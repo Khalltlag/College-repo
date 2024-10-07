4cdead909c267967b5da04a9db28e96d9cd61193
Miromaho <144359708+Miromaho@users.noreply.github.com>
2024-10-07 15:48:14 +0300

    Pw_8

---
+++ Pw 8/Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace OOP_Practice
+{
+    // Базовый класс
+    class Animal
+    {
+        // Свойства
+        public string Name { get; set; }
+        public int Age { get; set; }
+        // Конструктор
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        // Виртуальный метод, который можно переопределить в производных классах
+        public virtual void Eat() 
+        {
+            Console.WriteLine("Животное ест");
+        }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Животное издает звук.");
+        }
+
+        public virtual void Fetch() 
+        {
+            Console.WriteLine("Животное делает действие");
+        }
+    }
+}
