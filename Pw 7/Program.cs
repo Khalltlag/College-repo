c94079782f3bbba2b6568f1dc2f3c721ca2398d3
Miromaho <144359708+Miromaho@users.noreply.github.com>
2024-10-07 15:47:28 +0300

    Pw_7

---
+++ Pw 7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OOP_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            BankAccount myAccount = new BankAccount(123456, 1000, "Владелец счета");
+            
+            myAccount.Deposit(5000);
+            
+            myAccount.Withdraw(2000);
+            
+            myAccount.Withdraw(6000);
+            
+            Console.WriteLine($"Текущий баланс счета: {myAccount.Balance}");
+
+
+        }
+
+    }
+}
